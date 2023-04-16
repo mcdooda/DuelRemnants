acc8cd544dd2339b7958d1e7667677d9b6d8812c
Tom Rouillard <tom@rouillard.org>
2023-04-16 23:41:29 +0200

    add levels to consuming_rebirth weapon

--- player/ability/abilities/consuming_rebirth/consuming_rebirth.gd
+++ player/ability/abilities/consuming_rebirth/consuming_rebirth.gd
@@ -5,8 +5,12 @@ func _ready():
 	init_timer()
 
 func trigger_ability():
-	for i in num_projectiles:
+	for i in num_projectiles + modifiers.num_projectiles:
 		var projectile = projectile_scene.instantiate()
-		projectile.global_position = get_parent().global_position
+		projectile.global_position = get_parent().global_position + Vector2(0, i * 50)
 		projectile.direction = get_parent().direction
+		projectile.direction.y = 0
+		if projectile.direction.x == 0:
+			projectile.direction.x = 1
 		get_tree().root.add_child(projectile)
+
